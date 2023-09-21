257ebe1193e75eaa09112beb48bb0170cb3c8353
Gregory Higley <greg@prosumma.com>
2023-09-20 23:14:11 -0400

    Initial Commit

---
+++ Permanforce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Permanforce
+//
+//  Created by Greg Higley on 2023-09-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Permanforce/PermanforceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PermanforceApp.swift
+//  Permanforce
+//
+//  Created by Greg Higley on 2023-09-20.
+//
+
+import SwiftUI
+
+@main
+struct PermanforceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
